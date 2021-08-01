53f29eb9a1a11ceb12c223b9ab4f5bba4d51be91
Roj Maharjan <84655184+rojmaharjan@users.noreply.github.com>
2021-08-01 15:17:07 +0545

    Add files via upload

---
+++ 1helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+(The header file stdio.h defined as Standard Input utput.)
+(int means integer number.)
+(main(){
+} means main function code entry.)
+(printf(""); it means to print the function.)
+(retun o; it means the integer no.)
+*/
+//=(this is a way to write a comment in c progamming.)
+int main(){
+	
+	printf("helloworld\n\nHello Everyone");
+	return 0;
+}
+
+
